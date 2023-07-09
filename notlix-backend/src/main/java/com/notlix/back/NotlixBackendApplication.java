ff38793464911de942f16148796427d0d71c3e61
Feliixx0803 <felixperezi3p@gmail.com>
2023-07-09 22:51:55 +0200

    front y back desde 0

---
+++ notlix-backend/src/main/java/com/notlix/back/NotlixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.notlix.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotlixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotlixBackendApplication.class, args);
+	}
+
+}
---
+++ notlix-backend/src/test/java/com/notlix/back/NotlixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notlix.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotlixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
