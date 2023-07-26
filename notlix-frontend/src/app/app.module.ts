b67dabb1c52324fd36518fbb43a7f9b19c02f016
Feliixx0803 <felixperezi3p@gmail.com>
2023-07-26 13:17:29 +0200

    Añadido componente 404 y creado componente vacio login. Actualizado appRouting Añadidos import en styles.scss para estilos angular material e iconos de google

--- notlix-frontend/src/app/app.module.ts
+++ notlix-frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import {NoPageFoundComponent} from "./errorPage/no-page-found/no-page-found.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NoPageFoundComponent
   ],
   imports: [
     BrowserModule,
